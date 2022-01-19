b82df93288f2b7f92f76e479eff52b715e133fd2
Lucas Barbosa <lucas.barbosa@adentis.pt>
2022-01-19 20:58:05 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Faq from "./Faq";
+import Header from "./Header";
 
-function App() {
+const App = () => {
+  const [faqs, setFaqs] = useState([
+    {
+      question: "How many programmers does it take to screw in a lightbulb?",
+      answer: "None. We don't address hardware issues.",
+      open: true,
+    },
+    {
+      question: "Who is the most awesome person?",
+      answer: "You, the viewer.",
+      open: false,
+    },
+    {
+      question: "FAQ?",
+      answer: "Oh yeah. FAQ.",
+      open: false,
+    },
+  ]);
+
+  const toggleFaq = (faqIndex) => {
+    setFaqs(
+      faqs.map((faq, index) => {
+        if (index === faqIndex) {
+          faq.open = !faq.open;
+        } else {
+          faq.open = false;
+        }
+
+        return faq;
+      })
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <div className="faqs">
+        {faqs.map((faq, index) => (
+          <Faq faq={faq} index={index} toggleFaq={toggleFaq} />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>FAQ Page</h1>
+    </header>
+  );
+};
+
+export default Header;
